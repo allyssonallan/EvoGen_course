8bf32ca44ba624fc3135132afa8c7edc182f407f
mfumagalli <oceanodigomma@gmail.com>
2015-04-01 09:55:07 +0100

    add tmix

--- Scripts/plotTreemix.R
+++ Scripts/plotTreemix.R
@@ -4,7 +4,7 @@ tmix=args[1]
 fin=args[2]
 rm(args)
 
-source(paste(tmix,"/src/plotting_funcs.R", sep="",collapse="")
+source(paste(tmix,"/src/plotting_funcs.R", sep="",collapse=""))
 pdf(paste(fin,".pdf",sep="",collapse=""))
 par(mfrow=c(1,2))
 plot_tree(fin)
