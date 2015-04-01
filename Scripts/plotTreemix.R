80b2359b4b6f1e4448d531cb9b1ed5da6a0e12aa
mfumagalli <oceanodigomma@gmail.com>
2015-04-01 09:53:21 +0100

    add tmix

--- Scripts/plotTreemix.R
+++ Scripts/plotTreemix.R
@@ -1,11 +1,14 @@
 
-fin=commandArgs(T)[1]
+args=commandArgs(T)
+tmix=args[1]
+fin=args[2]
+rm(args)
 
-source("Software/treemix-1.12/src/plotting_funcs.R");
+source(paste(tmix,"/src/plotting_funcs.R", sep="",collapse="")
 pdf(paste(fin,".pdf",sep="",collapse=""))
 par(mfrow=c(1,2))
 plot_tree(fin)
-plot_resid(fin, "Data/poporder.txt")
+plot_resid(fin, "../Data/poporder.txt")
 dev.off()
 
 
